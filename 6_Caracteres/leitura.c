6e1af06411c07bda4873a39172bfb404200ec53b
Saori Kodama <115411538+SaoriKodama@users.noreply.github.com>
2023-07-04 14:32:41 +0000

    add

---
+++ 6_Caracteres/leitura.c
@@ -0,0 +1,13 @@
+/*
+ * Leitura de caracteres/ Leitura do valor do caracter.
+ * Tabela ASCII: https://en.wikipedia.org/wiki/ASCII
+*/
+
+#include <stdio.h>
+
+int main() {
+  char c, d;
+  scanf(" %c", &c);
+  printf("c = '%c' / c = '%d' / c = '%c'\n", c, c, 'd');
+  return 0;
+}
